b2d0fe428dc0fad4b4dd7bfef1558bfbed0e1a4f
ss0yaa <syp0205@sookmyung.ac.kr>
2025-12-13 22:12:11 +0900

    Feat/#12/MonthSection 컴포넌트 완성

--- src/pages/training/TrainingResultPage.jsx
+++ src/pages/training/TrainingResultPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SideNavBar from '@/components/common/SideNavBar'
+import MainResult from '@/components/training/MainResult'
 
 const TrainingResultPage = () => {
   return (
     <div className='flex w-full h-dvh'>
       <SideNavBar />
+      <MainResult />
     </div>
   )
 }
