7de06bec0492b511d21b525bd5d64e597bb8304e
ss0yaa <syp0205@sookmyung.ac.kr>
2025-12-13 16:16:33 +0900

    Feat/#12/버튼에 라우팅 경로 연결

--- src/components/training/ResultButton.jsx
+++ src/components/training/ResultButton.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Button from '@/assets/icons/icon-result.svg'
 
-const ResultButton = () => {
+function ResultButton({ onClick }) {
   return (
-    <div className='w-fit flex flex-row gap-[5px] m-auto justify-center items-center px-[63px] py-[11px] bg-primary rounded-[20px] cursor-pointer'>
+    <div
+      onClick={onClick}
+      className='w-fit flex flex-row gap-[5px] m-auto justify-center items-center px-[63px] py-[11px] bg-primary rounded-[20px] cursor-pointer'
+    >
       <img src={Button} />
       <p className='font-semibold text-white text-[20px]'>지난 결과 보기</p>
     </div>
