d40bb7db59f09801e69eff19d8c049ad2665ac4e
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-12-19 12:00:06 +0900

    feat/#23/대시보드 컨텐츠 컴포넌트

--- src/components/dashboard/DashboardContent.jsx
+++ src/components/dashboard/DashboardContent.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
 import ConditionCard from './ConditionCard'
 import StatusCard from './StatusCard'
+import QuizCard from './QuizCard'
+import CalenderCard from './CalenderCard'
+import NotificationCard from './NotificationCard'
+import InfoCard from './InfoCard'
 
 const DashboardContent = () => {
   return (
     <div className='px-[50px] py-[30px] w-full'>
       <div>
-        <h1 className='font-semibold text-[40px] text-black'>종합 대시보드</h1>
+        <h1 className='font-semibold text-[40px] text-black pb-6'>종합 대시보드</h1>
       </div>
-      <div>
+      <div className='grid grid-rows-3 gap-9'>
         <div className='grid grid-cols-4 gap-7'>
           <ConditionCard />
           <StatusCard theme={'medicine'} />
           <StatusCard theme={'diary'} />
           <StatusCard theme={'training'} />
         </div>
-        <div>{/* 중간영역 */}</div>
-        <div>{/* 하단 영역 */}</div>
+        <div className='grid grid-cols-3 gap-7'>
+          <QuizCard />
+          <CalenderCard />
+          <NotificationCard />
+        </div>
+        <div>
+          <InfoCard />
+        </div>
       </div>
     </div>
   )
