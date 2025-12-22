017e9957e6b08a69adb416242172138eca97c990
ss0yaa <syp0205@sookmyung.ac.kr>
2025-12-22 13:33:33 +0900

    Remove/#33/기사 더미데이터 삭제

--- src/components/training/BigArticleSection.jsx
+++ src/components/training/BigArticleSection.jsx
@@ -2,28 +2,6 @@ import React from 'react'
 import BigArticleCard from './BigArticleCard'
 import Thumb from '@/assets/images/temp-article-thumb.svg'
 
-// 더미데이터
-const dummyData = [
-  {
-    id: 1,
-    img: Thumb,
-    title: '뉴스 더미 데이터 1',
-    time: 3,
-  },
-  {
-    id: 2,
-    img: Thumb,
-    title: '뉴스 더미 데이터 2',
-    time: 3,
-  },
-  {
-    id: 3,
-    img: Thumb,
-    title: '뉴스 더미 데이터 3',
-    time: 3,
-  },
-]
-
 const BigArticleSection = () => {
   return (
     <div className='w-full overflow-x-auto'>
--- src/components/training/SmallArticleSection.jsx
+++ src/components/training/SmallArticleSection.jsx
@@ -2,45 +2,6 @@ import React from 'react'
 import SmallArticleCard from './SmallArticleCard'
 import Thumb from '@/assets/images/temp-content-thumb.svg'
 
-// 더미데이터
-const dummyData = [
-  {
-    id: 1,
-    img: Thumb,
-    title: '뉴스 더미 데이터 1',
-    subtitle: '더미데이터입니다.',
-    time: 2,
-  },
-  {
-    id: 2,
-    img: Thumb,
-    title: '뉴스 더미 데이터 2',
-    subtitle: '더미데이터입니다.',
-    time: 2,
-  },
-  {
-    id: 3,
-    img: Thumb,
-    title: '뉴스 더미 데이터 3',
-    subtitle: '더미데이터입니다.',
-    time: 2,
-  },
-  {
-    id: 4,
-    img: Thumb,
-    title: '뉴스 더미 데이터 4',
-    subtitle: '더미데이터입니다.',
-    time: 2,
-  },
-  {
-    id: 5,
-    img: Thumb,
-    title: '예시 기사 더미 데이터 5',
-    subtitle: '더미데이터입니다.',
-    time: 2,
-  },
-]
-
 const SmallArticleSection = () => {
   return (
     <div className='w-full overflow-x-auto'>
