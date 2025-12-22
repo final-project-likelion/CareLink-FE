851aaa7af03a4cde508eea9aa2eeee4d99c9f79f
ss0yaa <syp0205@sookmyung.ac.kr>
2025-12-22 17:56:43 +0900

    Api/#37/기사요약 답변 제출 연동

--- src/components/training/ArticleBox.jsx
+++ src/components/training/ArticleBox.jsx
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react'
 import api from '@/apis/axios'
 import SummaryBox from './SummaryBox'
 import SummaryOneLineBox from './SummaryOneLineBox'
+import Loading from '../common/Loading'
 
 function ArticleBox({ newsId }) {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
   const getArticle = async () => {
     try {
+      setIsLoading(true)
       const res = await api.get(`/api/trainings/news/${newsId}`)
 
       if (res.data.success) {
@@ -17,22 +20,31 @@ function ArticleBox({ newsId }) {
       }
     } catch (err) {
       console.error(err)
+    } finally {
+      setIsLoading(false)
     }
   }
 
   useEffect(() => {
     getArticle()
-  }, [])
+  }, [newsId])
   return (
-    <div className='p-[30px] bg-white rounded-[20px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] '>
-      <div className='flex flex-col gap-[50px]'>
-        <div className='flex flex-col gap-2.5'>
-          <p className='font-semibold text-[30px] whitespace-pre-line'>{title}</p>
-          <p className='text-[20px] whitespace-pre-line leading-relaxed'>{content}</p>
+    <div className='relative'>
+      {isLoading && (
+        <div className='absolute inset-0 z-10 flex items-center justify-center'>
+          <Loading />
         </div>
-        <SummaryBox />
-        <hr className='w-[880px] border-0 border-t border-[#606060]' />
-        <SummaryOneLineBox />
+      )}
+      <div className='p-[30px] bg-white rounded-[20px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] '>
+        <div className='flex flex-col gap-[50px]'>
+          <div className='flex flex-col gap-2.5'>
+            <p className='font-semibold text-[30px] whitespace-pre-line'>{title}</p>
+            <p className='text-[20px] whitespace-pre-line leading-relaxed'>{content}</p>
+          </div>
+          <SummaryBox newsId={newsId} />
+          <hr className='w-[880px] border-0 border-t border-[#606060]' />
+          <SummaryOneLineBox newsId={newsId} />
+        </div>
       </div>
     </div>
   )
