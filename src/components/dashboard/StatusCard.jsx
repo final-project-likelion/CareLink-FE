f0a9b0853113482d11f15cec61e985ce2db63c48
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-12-23 19:01:51 +0900

    feat/#41/원형 그래프 대시보드 컴포넌트 추가

--- src/components/dashboard/StatusCard.jsx
+++ src/components/dashboard/StatusCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import medicine from '@/assets/icons/icon-dashboard-medicine.svg'
 import diary from '@/assets/icons/icon-dashboard-dairy.svg'
 import training from '@/assets/icons/icon-dashboard-training.svg'
+import CircularProgressChart from './CircularProgressChart'
 
 const StatusCard = ({ theme }) => {
   const STATUS_THEME = {
@@ -10,22 +11,25 @@ const StatusCard = ({ theme }) => {
       img: medicine,
       bg: '#E3EAF7',
       iconBg: '#C1D5F6',
+      stroke: '#1990E6',
     },
     diary: {
       title: '일기 작성',
       img: diary,
       bg: '#F5EFDF',
       iconBg: '#F3E3B4',
+      stroke: '#EAB308',
     },
     training: {
       title: '인지 훈련',
       img: training,
       bg: '#EEE6F7',
       iconBg: '#E0C9F6',
+      stroke: '#A855F7',
     },
   }
 
-  const { title, img, bg, iconBg } = STATUS_THEME[theme]
+  const { title, img, bg, iconBg, stroke } = STATUS_THEME[theme]
 
   return (
     <div className='rounded-[15px] p-[21px] min-w-40' style={{ backgroundColor: bg }}>
@@ -39,12 +43,15 @@ const StatusCard = ({ theme }) => {
           />
         </div>
       </div>
-      <div>
-        <p className='font-semibold text-[28px] text-black py-5'>완료</p>
-        {/* <img src="" alt="" /> */}
-        <p className='font-normal text-[15px] text-black text-end'>
-          이번 주 <br /> 5/7회{' '}
-        </p>
+      <div className='flex pt-[50px]'>
+        <CircularProgressChart value={20} color={stroke} />
+        <div className='pl-3.5'>
+          <p className='font-semibold text-[28px] text-black'>완료</p>
+          <p className='font-normal text-[15px] text-black '>
+            이번 주 <br />
+            5/7회{' '}
+          </p>
+        </div>
       </div>
     </div>
   )
