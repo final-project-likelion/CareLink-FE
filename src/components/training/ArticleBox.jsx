5ec949bd1247f7f17a43f7c6d501e6c8ecb0ef52
ss0yaa <syp0205@sookmyung.ac.kr>
2025-12-25 01:02:50 +0900

    Feat/#37/기존 컴포넌트 이용해 특정 기사 결과 페이지 완성

--- src/components/training/ArticleBox.jsx
+++ src/components/training/ArticleBox.jsx
@@ -4,19 +4,31 @@ import SummaryBox from './SummaryBox'
 import SummaryOneLineBox from './SummaryOneLineBox'
 import Loading from '../common/Loading'
 
-function ArticleBox({ newsId }) {
+function ArticleBox({ newsId, mode }) {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const [userSixW, setUserSixW] = useState(null)
+  const [systemSixW, setSystemSixW] = useState(null)
+  const [userSummary, setUserSummary] = useState(null)
+  const [systemSummary, setSystemSummary] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
 
   const getArticle = async () => {
     try {
       setIsLoading(true)
-      const res = await api.get(`/api/trainings/news/${newsId}`)
 
-      if (res.data.success) {
+      if (mode === 'today') {
+        const res = await api.get(`/api/trainings/news/${newsId}`)
         setTitle(res.data.data.title)
         setContent(res.data.data.content)
+      } else {
+        const res = await api.get(`/api/trainings/${newsId}`)
+        setTitle(res.data.data.newsTitle)
+        setContent(res.data.data.newsContent)
+        setUserSixW(res.data.data.userSixW)
+        setSystemSixW(res.data.data.systemSixW)
+        setUserSummary(res.data.data.userSummary)
+        setSystemSummary(res.data.data.systemSummary)
       }
     } catch (err) {
       console.error(err)
@@ -27,7 +39,7 @@ function ArticleBox({ newsId }) {
 
   useEffect(() => {
     getArticle()
-  }, [newsId])
+  }, [newsId, mode])
   return (
     <div className='relative'>
       {isLoading && (
@@ -41,9 +53,23 @@ function ArticleBox({ newsId }) {
             <p className='font-semibold text-[30px] whitespace-pre-line'>{title}</p>
             <p className='text-[20px] whitespace-pre-line leading-relaxed'>{content}</p>
           </div>
-          <SummaryBox newsId={newsId} />
-          <hr className='w-[880px] border-0 border-t border-[#606060]' />
-          <SummaryOneLineBox newsId={newsId} />
+          {mode === 'today' ? (
+            <>
+              <SummaryBox newsId={newsId} />
+              <hr className='w-[880px] border-0 border-t border-[#606060]' />
+              <SummaryOneLineBox newsId={newsId} />
+            </>
+          ) : (
+            <>
+              <SummaryBox mode='history' defaultFieldData={userSixW} defaultAnswers={systemSixW} />
+              <hr className='w-[880px] border-0 border-t border-[#606060]' />
+              <SummaryOneLineBox
+                mode='history'
+                defaultFieldData={userSummary}
+                defaultAnswer={systemSummary}
+              />
+            </>
+          )}
         </div>
       </div>
     </div>
