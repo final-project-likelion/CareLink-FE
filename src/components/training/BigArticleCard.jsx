7bc366aeb0d32f0fc9b756f53770138927011b18
ss0yaa <syp0205@sookmyung.ac.kr>
2025-12-25 14:22:18 +0900

    Refactor/#50/뉴스 목록 조회 페이지에 토스트 메세지 추가

--- src/components/training/BigArticleCard.jsx
+++ src/components/training/BigArticleCard.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
+import ToastModal from '../signup/ToastModal'
 
 function BigArticleCard({ newsId, img, title, time, disabled }) {
+  const [toast, setToast] = useState({
+    open: false,
+    message: '',
+  })
   const navigate = useNavigate()
 
   const handleClick = () => {
     if (disabled) {
-      alert('오늘의 문제는 이미 완료하셨어요. 내일 다시 참여해 주세요.')
+      showToast('오늘의 문제는 이미 완료하셨어요. 내일 다시 참여해 주세요.')
       return
     }
     navigate(`/training/article/${newsId}`)
   }
+
+  const showToast = (msg) => {
+    setToast({
+      open: true,
+      message: msg,
+    })
+  }
+
+  const closeToast = () => {
+    setToast({
+      open: false,
+      message: '',
+    })
+  }
   return (
-    <div
-      onClick={handleClick}
-      className='w-80 h-80 flex flex-col gap-2.5 p-5 rounded-[20px] bg-[#E8E8E8] cursor-pointer'
-    >
-      <img className='w-[280px] h-[170px] rounded-2xl object-cover pb-2.5' src={img} />
-      <p className='font-semibold text-[20px] line-clamp-1'>{title}</p>
-      <p className='font-normal text-[18px]'>예상 소요 시간: {time}분</p>
-    </div>
+    <>
+      <div
+        onClick={handleClick}
+        className='w-80 h-80 flex flex-col gap-2.5 p-5 rounded-[20px] bg-[#E8E8E8] cursor-pointer'
+      >
+        <img className='w-[280px] h-[170px] rounded-2xl object-cover pb-2.5' src={img} />
+        <p className='font-semibold text-[20px] line-clamp-1'>{title}</p>
+        <p className='font-normal text-[18px]'>예상 소요 시간: {time}분</p>
+      </div>
+      <ToastModal open={toast.open} message={toast.message} duration={1500} onClose={closeToast} />
+    </>
   )
 }
 
--- src/components/training/SmallArticleCard.jsx
+++ src/components/training/SmallArticleCard.jsx
@@ -1,26 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import ToastModal from '../signup/ToastModal'
 
 const SmallArticleCard = ({ newsId, img, title, subtitle, time, disabled }) => {
+  const [toast, setToast] = useState({
+    open: false,
+    message: '',
+  })
   const navigate = useNavigate()
 
   const handleClick = () => {
     if (disabled) {
-      alert('오늘의 문제는 이미 완료하셨어요. 내일 다시 참여해 주세요.')
+      showToast('오늘의 문제는 이미 완료하셨어요. 내일 다시 참여해 주세요.')
       return
     }
     navigate(`/training/article/${newsId}`)
   }
+
+  const showToast = (msg) => {
+    setToast({
+      open: true,
+      message: msg,
+    })
+  }
+
+  const closeToast = () => {
+    setToast({
+      open: false,
+      message: '',
+    })
+  }
   return (
-    <div
-      onClick={handleClick}
-      className='w-64 h-80 flex flex-col  gap-2.5 py-5 px-[15px] rounded-2xl bg-[#E8E8E8] cursor-pointer'
-    >
-      <img className='w-[219px] h-[110px] rounded-2xl object-coverpb-2.5' src={img} />
-      <p className='font-semibold text-[20px] pb-[5px] line-clamp-1'>{title}</p>
-      <p className='font-normal text-[18px] line-clamp-2'>{subtitle}</p>
-      <p className='font-normal text-[18px]'>예상 소요 시간: {time}분</p>
-    </div>
+    <>
+      <div
+        onClick={handleClick}
+        className='w-64 h-80 flex flex-col  gap-2.5 py-5 px-[15px] rounded-2xl bg-[#E8E8E8] cursor-pointer'
+      >
+        <img className='w-[219px] h-[110px] rounded-2xl object-coverpb-2.5' src={img} />
+        <p className='font-semibold text-[20px] pb-[5px] line-clamp-1'>{title}</p>
+        <p className='font-normal text-[18px] line-clamp-2'>{subtitle}</p>
+        <p className='font-normal text-[18px]'>예상 소요 시간: {time}분</p>
+      </div>
+      <ToastModal open={toast.open} message={toast.message} duration={1500} onClose={closeToast} />
+    </>
   )
 }
 
